22337bb55a1c3ad700cb1ff744e942694d4ab23d
Ayush <daskumarayush@gmail.com>
2024-07-28 15:03:52 +0530

    day1

---
+++ FindTheDuplicateNumber.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int findDuplicate(vector<int>& nums) {
+        
+        int fast = 0;
+        int slow = 0;
+
+        while(true){
+            fast = nums[nums[fast]];
+            slow = nums[slow];
+            if(fast == slow){
+                break;
+            }
+        }
+        slow = 0;
+        while(true){
+            fast = nums[fast];
+            slow = nums[slow];
+            if(fast == slow){
+                break;
+            }
+        }
+
+        return slow;
+    }
+};
